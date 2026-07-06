d692c617d3904a746282c60417d302795546f6e3
StenixDev <stenix.dev@gmail.com>
2026-07-06 11:39:14 +0800

    [MWA] - update layout

--- mwa/app/layout.js
+++ mwa/app/layout.js
@@ -1,15 +1,12 @@
-import type { Metadata } from 'next';
 import './globals.css';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Military Widows Advocacy',
   description:
     'Behind every fallen hero is often a spouse whose life has been forever changed. We provide employment opportunities, business startup assistance, emergency financial aid for urgent bills, and other resources designed to bring hope and practical help.',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>{children}</body>
